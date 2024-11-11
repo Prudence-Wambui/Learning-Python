7fb6624576b9812934b348f3a6b181d3a4951e00
Prudence-Wambui <prudencewambui.official@gmail.com>
2024-11-11 20:22:49 +0300

    Commit

---
+++ variables.py
@@ -0,0 +1,19 @@
+# A variable is a container of a value and it behaves as the value that it contains.
+
+# Strings
+name = "Prudence"
+first_name = "Prudence"
+last_name = "Wambui"
+full_name = first_name  +  last_name  
+full_name1 = first_name  + " " + last_name  
+
+print("Hello "+full_name)
+print("Hello "+full_name1)
+
+#Print the data type of a variable
+print(type(name))
+
+print("Hello "+name) # Make sure that when you are printing a variable you do not use quotes.
+
+#Integers
+age = 21 #if you place your number within quotes, it becomes a string.
